f124c5e7695814a606c76c703b10f172fa22e8f0
DominicDev <s0l0estudi4r@gmail.com>
2025-11-09 22:09:37 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'KANA-BOON';
+export const nameSongs = [
+	"SILHOUETTE", /*1*/
+	"DIVER", /*2*/
+	"NAI MONO NEDARI", /*3*/
+	"STARMARK", /*4*/
+	"FULL DRIVE", /*5*/
+	"NAMIDA", /*6*/
+	" IKITE YUKU", /*7*/
+	"FIGHTER", /*8*/
+	"SAKURA NO UTA ", /*9*/
+	"BATON ROAD ", /*10*/
+	"MUSIC", /*11*/
+	"STEP TO YOU", /*12*/
+	"AI NI MUKATTE ", /*13*/
+	"LOLLIPOP ", /*14*/
+	"NANDEMO NEDARI", /*15*/
+	"TIME", /*16*/
+	"MASSARA", /*17*/
+	"SPIRAL", /*18*/
+	"HAGURUMA", /*19*/
+	"TALKING", /*20*/
+]
